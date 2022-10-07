1005214d1b504306d3e6db2211b14a34a2781b42
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-10-07 20:06:13 +0300

    Task_04

---
+++ Folder_04/a list of N elements.py
@@ -0,0 +1,29 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. 
+# Позиции хранятся в файле file.txt в одной строке одно число.
+
+from random import randint
+
+def get_numbers(n):
+    return [randint(-n/2, n) for i in range(-n, n + 1)]
+
+def get_data_from_file(path):
+    data = open(path,)
+    dlist = [int(line.strip()) for line in data]
+    data.close()
+    return dlist
+
+def get_mult(numbers, datalist):
+    mult = 1
+    for i in datalist:
+        mult *= numbers[i]
+    return mult
+
+path = 'Mult_task_1.txt'
+n = 10 
+datalist = get_data_from_file(path)
+numbers = get_numbers(n)
+
+print(numbers)
+print(datalist)
+print(get_mult(numbers, datalist))
