6dbea6977eb9d1964f19f3a657b0c73794073286
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-10-07 20:08:18 +0300

    Task_05

---
+++ Folder_05/List shuffing algorithm.py
@@ -0,0 +1,9 @@
+# Реализуйте алгоритм перемешивания списка.
+
+import random
+list = [1, 2 , 3, 4, 5, 6, 7, 8, 9]
+print("Исходный список: ")
+print(list)
+random.shuffle(list)
+print("Перемешанный список: ")
+print(list)
